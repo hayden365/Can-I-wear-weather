40ed300fc30531556ef66ee3e89df0bd31231a84
hayden365 <ysmh100@gmail.com>
2023-04-08 00:45:44 +0900

    Fea: Header작성

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+type Props = {};
+
+const Header = (props: Props) => {
+	return (
+		<section className="h-wrapper">
+			<div className="h-container">
+				<img src="./green_logo.png" alt="logo" width={100} />
+
+				<div className="h-menu">
+					<a href="">Tree</a>
+					<a href="">Grass</a>
+					<a href="">Flower</a>
+					<button>
+						<a href="">Contact</a>
+					</button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Header;
