26b933435600ef6736e094d0e6181089096dc21a
hayden365 <ysmh100@gmail.com>
2023-04-09 04:50:53 +0900

    Fet: Hero Html

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,14 +6,13 @@ type Props = {};
 const Header = (props: Props) => {
 	return (
 		<section className="h-wrapper">
-			<div className="flexCenter paddings innerWidth">
+			<div className="flexCenter innerWidth paddings">
 				<img src="./green_logo.png" alt="logo" width={100} />
-
-				<div className="h-menu">
+				<div className="flexCenter h-menu">
 					<a href="">Tree</a>
 					<a href="">Grass</a>
 					<a href="">Flower</a>
-					<button>
+					<button className="button">
 						<a href="">Contact</a>
 					</button>
 				</div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,17 @@ import React from "react";
 type Props = {};
 
 const Hero = (props: Props) => {
-	return <div>Hero</div>;
+	return (
+		<section className="hero-wrapper">
+			<div className="paddings innerWidth flexCenter hero-container">
+				<div className="hero-left"></div>
+
+				<div className="flexCenter hero-right">
+					<div className="image-container"></div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
